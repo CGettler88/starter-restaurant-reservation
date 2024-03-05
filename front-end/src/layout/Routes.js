e7c801550e2763bc12a1fbcb67966ecfe54b250d
Cristina Gettler <135405049+CGettler88@users.noreply.github.com>
2024-03-05 12:44:09 -0700

    Add files via upload

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,13 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import NewReservation from "../reservations/NewReservation";
+import EditReservation from "../reservations/EditReservation";
+import NewTable from "../tables/NewTable";
+import Seat from "../reservations/Seat";
+import Search from "../search/Search";
 
 /**
  * Defines all the routes for the application.
@@ -12,6 +16,7 @@ import { today } from "../utils/date-time";
  *
  * @returns {JSX.Element}
  */
+
 function Routes() {
   return (
     <Switch>
@@ -23,6 +28,21 @@ function Routes() {
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
+      </Route>
+      <Route path="/reservations/new">
+        <NewReservation />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <EditReservation />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <Seat />
+      </Route>
+      <Route path="/tables/new">
+        <NewTable />
+      </Route>
+      <Route path="/search">
+        <Search />
       </Route>
       <Route>
         <NotFound />
