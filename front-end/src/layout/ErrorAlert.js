d77d11b8773c6738d699ed4c90811e43b7d64d7e
Cristina Gettler <CristinaABBC@outlook.com>
2024-03-06 15:57:11 -0700

    changes

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 /**
@@ -24,30 +23,3 @@ function ErrorAlert({ error }) {
 }
 
 export default ErrorAlert;
-=======
-import React from "react";
-
-/**
- * Defines the alert message to render if the specified error is truthy.
- * @param error
- *  an instance of an object with `.message` property as a string, typically an Error instance.
- * @returns {JSX.Element}
- *  a bootstrap danger alert that contains the message string.
- */
-
-function ErrorAlert({ error }) {
-  if (error.length === 0) {
-    return null;
-  } else {
-    return error.map((err, index) => {
-      return (
-        <div key={index} className="alert alert-danger m-2">
-          Error: {err}
-        </div>
-      );
-    });
-  }
-}
-
-export default ErrorAlert;
->>>>>>> 0fbe0b3fb5eb4f240442edc188ce184422563833
